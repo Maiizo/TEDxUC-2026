55749ee3923eeaa7b43e187295a3c8aee3692269
Eileen <ecynthia01@student.ciputra.ac.id>
2026-03-13 00:28:50 +0700

    fix error for depl

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,9 +2,9 @@ import "dotenv/config";
 import { defineConfig } from "prisma/config";
 
 export default defineConfig({
-  schema: "prisma/schema.prisma",
+  schema: "lib/prisma/schema.prisma",
   migrations: {
-    path: "prisma/migrations",
+    path: "lib/prisma/migrations",
   },
   datasource: {
     url: process.env["DATABASE_URL"],
